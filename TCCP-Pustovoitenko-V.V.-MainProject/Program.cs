ea5cc70bc0923aacb3e6babcbef927e6fd12428c
Copying-Ninja <2001vlad2908@gmail.com>
2021-04-26 01:34:39 +0300

    Add project files.

---
+++ TCCP-Pustovoitenko-V.V.-MainProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCCP_Pustovoitenko_V.V._MainProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            double b = 2.66;
+            Console.WriteLine("a + b = " + (a+b) );
+            Console.WriteLine("Goodbay world:(");
+            Console.ReadKey();
+        }
+    }
+}
